8f021a9848a66534ebcbe8b70caff021a9e40a94
Josh R <joshriv122104@gmail.com>
2024-04-10 19:34:33 -0400

    created encode

---
+++ jnr_lab9.py
@@ -0,0 +1,27 @@
+# Joshua Rivera
+
+def encode(password):
+    new_password = ""
+    for char in password:
+        new_password += str(int(char)+3)
+    return new_password
+
+def main():
+    while True:
+        print("""
+        Menu
+        -------------
+        1. Encode
+        2. Decode
+        3. Quit
+        """)
+
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_p = encode(password)
+            print("Your password has been encoded and stored!")
+        elif choice == 2:
+            print(f"The encoded password is {encoded_p}, and the original password is {decode(encoded_p)}.")
+        elif choice == 3:
+            break
